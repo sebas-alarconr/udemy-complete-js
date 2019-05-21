abec03e45965c195e5962281fc99730bb1e354ae
Sebastián Alarcón <sebas.alarcon8@gmail.com>
2019-05-21 17:56:40 -0500

    Array section added.

--- 2-JS-basics/final/script.js
+++ 2-JS-basics/final/script.js
@@ -25,7 +25,7 @@ console.log(job);
 job = 'Teacher';
 console.log(job);
 
-// Variable naming rules
+// Variable naming rules.
 var _3years = 3;
 var johnMark = 'John and MArk';
 var if = 23;
@@ -55,7 +55,7 @@ isMarried = false;
 
 console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
 
-// Variable mutation
+// Variable mutation.
 age = 'twenty eight';
 job = 'driver';
 
@@ -75,7 +75,7 @@ now = 2018;
 ageJohn = 28;
 ageMark = 33;
 
-// Math operators
+// Math operators.
 yearJohn = now - ageJohn;
 yeahMark = now - ageMark;
 
@@ -86,12 +86,12 @@ console.log(now * 2);
 console.log(now / 10);
 
 
-// Logical operators
+// Logical operators.
 var johnOlder = ageJohn < ageMark;
 console.log(johnOlder);
 
 
-// typeof operator
+// typeof operator.
 console.log(typeof johnOlder);
 console.log(typeof ageJohn);
 console.log(typeof 'Mark is older tha John');
@@ -108,23 +108,23 @@ var now = 2018;
 var yearJohn = 1989;
 var fullAge = 18;
 
-// Multiple operators
+// Multiple operators.
 var isFullAge = now - yearJohn >= fullAge; // true
 console.log(isFullAge);
 
-// Grouping
+// Grouping.
 var ageJohn = now - yearJohn;
 var ageMark = 35;
 var average = (ageJohn + ageMark) / 2;
 console.log(average);
 
-// Multiple assignments
+// Multiple assignments.
 var x, y;
 x = y = (3 + 5) * 4 - 6; // 8 * 4 - 6 // 32 - 6 // 26
 console.log(x, y);
 
 
-// More operators
+// More operators.
 x *= 2;
 console.log(x);
 x += 10;
@@ -139,12 +139,16 @@ console.log(x);
 *********************************************/
 
 /*
-Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / height^2 = mass / (height * height). (mass in kg and height in meter).
+Mark and John are trying to compare their BMI (Body Mass Index), which is
+calculated using the formula: BMI = mass / height^2 = mass / (height * height).
+(mass in kg and height in meter).
 
 1. Store Mark's and John's mass and height in variables
 2. Calculate both their BMIs
-3. Create a boolean variable containing information about whether Mark has a higher BMI than John.
-4. Print a string to the console containing the variable from step 3. (Something like "Is Mark's BMI higher than John's? true").
+3. Create a boolean variable containing information about whether Mark has a
+higher BMI than John.
+4. Print a string to the console containing the variable from step 3.
+(Something like "Is Mark's BMI higher than John's? true").
 
 GOOD LUCK 😀
 */
@@ -228,8 +232,10 @@ if (age < 13) {
 var firstName = 'John';
 var age = 14;
 
-// Ternary operator
-age >= 18 ? console.log(firstName + ' drinks beer.') : console.log(firstName + ' drinks juice.');
+// Ternary operator.
+age >= 18 ?
+    console.log(firstName + ' drinks beer.') :
+    console.log(firstName + ' drinks juice.');
 
 var drink = age >= 18 ? 'beer' : 'juice';
 console.log(drink);
@@ -240,7 +246,7 @@ console.log(drink);
   var drink = 'juice';
 }
 
-// Switch statement
+// Switch statement.
 var job = 'instructor';
 switch (job) {
   case 'teacher':
@@ -279,7 +285,7 @@ switch (true) {
 *********************************************/
 /*
 // falsy values: undefined, null, 0, '', NaN
-// truthy values: NOT falsy values
+// truthy values: NOT falsy values.
 
 var height;
 
@@ -303,14 +309,22 @@ if (height === '23') {
 *********************************************/
 
 /*
-John and Mike both play basketball in different teams. In the latest 3 games, John's team scored 89, 120 and 103 points, while Mike's team scored 116, 94 and 123 points.
+John and Mike both play basketball in different teams. In the latest 3 games,
+John's team scored 89, 120 and 103 points, while Mike's team scored 116, 94 and
+123 points.
 
 1. Calculate the average score for each team
-2. Decide which teams wins in average (highest average score), and print the winner to the console. Also include the average score in the output.
-3. Then change the scores to show different winners. Don't forget to take into account there might be a draw (the same average score)
+2. Decide which teams wins in average (highest average score), and print the
+winner to the console. Also include the average score in the output.
+3. Then change the scores to show different winners. Don't forget to take into
+account there might be a draw (the same average score)
 
-4. EXTRA: Mary also plays basketball, and her team scored 97, 134 and 105 points. Like before, log the average winner to the console. HINT: you will need the && operator to take the decision. If you can't solve this one, just watch the solution, it's no problem :)
-5. Like before, change the scores to generate different winners, keeping in mind there might be draws.
+4. EXTRA: Mary also plays basketball, and her team scored 97, 134 and 105
+points. Like before, log the average winner to the console. HINT: you will need
+the && operator to take the decision. If you can't solve this one, just watch
+the solution, it's no problem :)
+5. Like before, change the scores to generate different winners, keeping in mind
+there might be draws.
 
 GOOD LUCK 😀
 */
@@ -375,10 +389,10 @@ yearsUntilRetirement(1969, 'Jane');
 * Function Statements and Expressions
 *********************************************/
 /*
-// Function declaration
+// Function declaration.
 // function whatDoYouDo(job, firstName) {}
 
-// Function expression
+// Function expression.
 var whatDoYouDo = function(job, firstName) {
   switch(job) {
     case 'teacher':
@@ -401,3 +415,41 @@ console.log(whatDoYouDo('retired', 'Mark'));
 /*********************************************
 * Arrays
 *********************************************/
+/*
+// Initialize new array.
+var names = ['John', 'Mark', 'Jane'];
+var years = new Array(1990, 1969, 1948);
+
+console.log(names[0]);
+console.log(names.length);
+
+// Mutate array data.
+names[1] = 'Ben';
+names[names.length] = 'Mary';
+console.log(names);
+
+// Different data types.
+var john = ['John', 'Smith', 1990, 'teacher', false];
+
+// Adding at the end of the array.
+john.push('blue');
+
+// Adding at the begining of the array.
+john.unshift('Mr.');
+console.log(john);
+
+// Remove last element.
+john.pop();
+
+// Remove first element.
+john.shift();
+console.log(john);
+
+// Know the position of a value. If -1 the value doesn't exist in the array.
+console.log(john.indexOf(1990));
+
+var isDesigner = john.indexOf('designer') === -1 ?
+    'John is not a designer' :
+    ' John is a designer.';
+console.log(isDesigner);
+*/
